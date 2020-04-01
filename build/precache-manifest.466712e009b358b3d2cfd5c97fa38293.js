8cbd7e897c62fa770005ade1a35e221eee28acc6
unknown <kindsonthegenius@gmail.com>
2020-04-01 17:12:23 +0200

    The first commit

---
+++ build/precache-manifest.466712e009b358b3d2cfd5c97fa38293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4871be3046792ef8938ba7a5267f4b83",
+    "url": "/ReactProjects/storage-app/build/index.html"
+  },
+  {
+    "revision": "e199d84684b975ca8677",
+    "url": "/ReactProjects/storage-app/build/static/css/main.2faf3c84.chunk.css"
+  },
+  {
+    "revision": "c3713518ba5b8325fbc8",
+    "url": "/ReactProjects/storage-app/build/static/js/2.b453dd33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactProjects/storage-app/build/static/js/2.b453dd33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e199d84684b975ca8677",
+    "url": "/ReactProjects/storage-app/build/static/js/main.000a6f01.chunk.js"
+  },
+  {
+    "revision": "1c69368b5c5ce63f3aaf",
+    "url": "/ReactProjects/storage-app/build/static/js/runtime-main.05f0a737.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Start from './Start'
+
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <Start></Start>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App></App>, document.getElementById("root"))
+
